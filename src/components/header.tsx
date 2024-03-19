44af88eaa316c7e0d02a469a9ef782abd4e75419
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-03-19 23:05:42 +0900

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -26,7 +26,7 @@ export default function Header() {
                         <span>DESIGNER MODE</span>
                     </MenuItem>
                     <MenuItem active="false">
-                        <span>PROJECT</span>
+                        <span>DEVELOPER MODE</span>
                     </MenuItem>
                 </MenuContainer>
                     <img src={FiroIcon} height={25} />
@@ -55,7 +55,7 @@ const HeaderContainer = styled.header`
 `
 
 const MenuContainer = styled.div`
-    width: 230px;
+    width: 280px;
     height: auto;
 
     display: flex;
